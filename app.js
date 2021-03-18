5df1a7ca475cc01d8ea77e16420a3a35abbd2fe1
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-03-18 09:45:07 +0530

    Updates log statements with chalk

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const chalk = require('chalk');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -7,10 +8,13 @@ const rl = readline.createInterface({
 
 const reverseGivenInput = () => {
     rl.question('Enter Input: \n', (input) => {
-        if (input === 'exit') return rl.close();
+        if (input === 'exit') {
+            console.log(chalk.red.inverse('Exiting the program!'));
+            return rl.close();
+        }
         else {
             const output = input.split("").reverse().join("");
-            console.log(`Output is: \n${output}`);
+            console.log(`Output is: \n${chalk.green.inverse(output)}`);
             reverseGivenInput();
         }
     });
