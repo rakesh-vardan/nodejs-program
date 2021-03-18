884e6195d421ad90f37fe10bf01fd4dc852d7f8d
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-03-18 14:58:01 +0530

    Updates the home task1.2

--- home-task1.2.js
+++ home-task1.2.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const csv = require('csvtojson');
 
-const csvFilePath = 'csv/nodejs-hw1-ex1.csv';
+const CSV_FILE_PATH = 'csv/nodejs-hw1-ex1.csv';
+const OUTPUT_FILE_NAME = 'output.txt';
 
 csv()
-    .fromFile(csvFilePath)
-    .then((jsonObj) => {
-        console.log(jsonObj);
-        fs.writeFileSync('output.txt', JSON.stringify(jsonObj), (error) => {
-            console.log('There is an error!' + error);
+    .fromFile(CSV_FILE_PATH)
+    .then((csvDataArray) => {
+        csvDataArray.forEach(element => {
+            const updatedElement = convertKeysToLowerCase(element);
+            writeToTextFile(updatedElement);
         });
     })
+
+const convertKeysToLowerCase = (element) => Object.fromEntries(
+    Object.entries(element).map(([k, v]) => [k.toLowerCase(), v])
+);
+
+const writeToTextFile = (element) => {
+    fs.appendFileSync(OUTPUT_FILE_NAME, JSON.stringify(element) + "\r\n", (error) => {
+        if (error) {
+            console.log('An error occured while writing the JSON object to file!');
+            return console.log(error);
+        }
+    });
+}
+
