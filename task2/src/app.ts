fe786a71ca247d8de55ec78f7662099d77739385
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-04-20 19:49:51 +0530

    Changes the port for server

--- task2/src/app.ts
+++ task2/src/app.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import userRoutes from './routes/users'
 
 const app = express();
+const port = parseInt(process.env.PORT || '3000')
 
 app.use(express.json())
 app.use(userRoutes);
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000...');
+app.listen(port, () => {
+    console.log(`Server started on port 3000 ${port}...`);
 });
 
